90086f5e89597436a4275fb7e40b6cb51f2978b9
Puneeth-elfonze <puneeth.gl@elfonze.com>
2025-05-09 17:56:22 +0530

    implemented discovery server

--- order-service/src/main/java/com/delivery/food_service/FoodServiceApplication.java
+++ order-service/src/main/java/com/delivery/food_service/FoodServiceApplication.java
@@ -2,8 +2,10 @@ package com.delivery.food_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FoodServiceApplication {
 
 	public static void main(String[] args) {
